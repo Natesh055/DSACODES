ba03e14385500e19d9f2e4db7f08b8e95338c5f2
natesh055 <natesh@Vijays-MacBook-Air.local>
2024-08-26 11:20:12 +0530

    Bugs fixed

--- PROJECTS/Spit Payment/tmep.cpp
+++ PROJECTS/Spit Payment/tmep.cpp
@@ -2,7 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main()
 {
     int n;
@@ -22,8 +21,6 @@ int main()
     }
     n = index;
 
-
-
     vector<vector<float>> exp(n, vector<float>(n, 0));
 
     int tamount = 0;
@@ -40,32 +37,30 @@ int main()
             continue;
         }
 
-        else //apne alava kisi or ka bhi bill de sakta hai
+        else // apne alava kisi or ka bhi bill de sakta hai
         {
 
-            for(int j=0;j<n;j++)
+            for (int j = 0; j < n; j++)
             {
-                if(i!=j)
+                if (i != j)
                 {
-                    cout<<j<<" ."<<v[j]<<endl;
+                    cout << j << ". " << v[j] << endl;
                 }
             }
             vector<pair<int, int>> temp;
 
-            int x=0;
+            int x = 0;
             int tempnum = n;
             tempnum--;
-            while(tempnum--) //input lene ke liye
+            while (tempnum--) // input lene ke liye
             {
-                if(x==-1)
-                break;
-                cout<<"Enter Number associated for the person payed for"<<endl;
-                cin>>x;
+                cout << "Enter Number associated for the person payed for, -1 if not payed for anyone" << endl;
+                cin >> x;
 
-                if(x==-1)
-                break;
+                if (x == -1)
+                    break;
 
-                cout<<"Enter amount payed for that person "<<v[x]<<endl;
+                cout << "Enter amount payed for " << v[x] << endl;
                 int a;
                 cin >> a;
                 temp.push_back({x, a});
@@ -77,7 +72,6 @@ int main()
             {
                 exp[i][it.first] = it.second;
             }
-
         }
     }
     for (int i = 0; i < n; i++)
@@ -99,7 +93,8 @@ int main()
     {
         int owe = tamount - exp[i][i];
         int get = 0;
-        vector<int> toget;
+        vector<pair<int, int>> toget;
+        vector<pair<int, int>> towe;
 
         if (exp[i][i] >= tamount)
         {
@@ -109,20 +104,29 @@ int main()
                 if (i != j && exp[i][j] != 0)
                 {
                     get += exp[i][j];
-                    toget.push_back(j);
+                    toget.push_back({exp[i][j], j});
                 }
             }
         }
-
-        int toown;
-        for (int j = 0; j < n; j++)
+        else
         {
-            if (exp[j][i] != 0)
+            for (int j = 0; j < n; j++)
             {
-                toown = j;
-                break;
+                if (i != j && exp[j][i] != 0)
+                {
+                    towe.push_back({exp[j][i], j});
+                }
             }
         }
+        // int toown;
+        // for (int j = 0; j < n; j++)
+        // {
+        //     if (exp[j][i] != 0)
+        //     {
+        //         toown = j;
+        //         break;
+        //     }
+        // }
 
         if (owe == 0 && get == 0)
         {
@@ -133,14 +137,23 @@ int main()
             cout << v[i] << " Does not owe any one and will get money from " << endl;
             for (auto it : toget)
             {
-                cout << v[it] << " Amount " << tamount << endl;
+                cout << v[it.second] << " Amount " << it.first << endl;
             }
         }
         else
         {
-            cout << v[i] << " Owes " << v[toown] << " Amount " << tamount-exp[i][i] << endl;
+            cout << v[i] << " Owes "<< endl;
+            // cout << v[i] << " Owes " << v[toown] << " Amount " << tamount - exp[i][i] << endl;
+
+            for (auto it : towe)
+            {
+                cout << v[it.second] << " Amount " << it.first << endl;
+            }
         }
         cout << endl;
         cout << endl;
     }
 }
+
+// 4 Natesh Varun Shivansh Piyush 230 2 30 3 100 -1 100 -1 70 -1 0
+// 4 Natesh Varun Shivansh Piyush 115 2 15 -1 100 -1 70 -1 115 2 15 -1
